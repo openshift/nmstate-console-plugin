9845c6581931ba170fc40794395fb74921f9fdc7
Aviv Turgeman <aturgema@redhat.com>
2024-12-25 10:32:14 +0200

    CNV-41967: display nncp configred interfaces
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/states/topology/components/CustomNode/CustomNode.tsx
+++ src/views/states/topology/components/CustomNode/CustomNode.tsx
@@ -25,11 +25,27 @@ const CustomNode: FC<CustomNodeProps> = ({ element, ...rest }) => {
 
   const xCenter = (width - ICON_SIZE) / 2;
   const yCenter = (height - ICON_SIZE) / 2;
+
+  const isDerivedFromPolicy = data.isDerivedFromPolicy;
   return (
     <DefaultNode className="custom-node" element={element} truncateLength={8} {...rest}>
       <g transform={`translate(${xCenter}, ${yCenter})`}>
         <Icon width={ICON_SIZE} height={ICON_SIZE} />
       </g>
+      {isDerivedFromPolicy && (
+        <rect
+          className="custom-node__inner-border"
+          x={8}
+          y={8}
+          width={width - 16}
+          height={height - 16}
+          rx="50%"
+          ry="50%"
+          stroke="#007BFF"
+          strokeWidth={2}
+          fill="none"
+        />
+      )}
     </DefaultNode>
   );
 };
