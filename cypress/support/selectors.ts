e34c31c47383f52a9c482e8f8078cdbfb1251924
Ugo Palatucci <upalatuc@redhat.com>
2025-02-19 14:33:41 +0100

    Fix tests

--- cypress/support/selectors.ts
+++ cypress/support/selectors.ts
@@ -3,5 +3,4 @@ export const EXPAND_INTERFACE_INFO = 'ethernet-expandable-section-toggle';
 export const INTERFACE_DRAWER_TEST_ID = 'interface-drawer';
 export const LLDP_DRAWER_DETAILS_SECTION_TEST_ID = 'lldp-section';
 
-export const SEARCH_FILTER_DROPDOWN =
-  'button.pf-v5-c-dropdown__toggle[data-test-id="dropdown-button"]';
+export const SEARCH_FILTER_DROPDOWN = 'button[data-test-id="dropdown-button"]';
