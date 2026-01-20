189b9267d22261be90dfad865ab2f7e833020615
Ugo Palatucci <upalatuc@redhat.com>
2026-01-20 17:24:03 +0100

    add i18n scripts to translate into all languages

---
+++ i18next-parser.config.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { CustomJSONLexer } = require('./i18n-scripts/lexers');
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  createOldCatalogs: false,
+  defaultNamespace: 'plugin__nmstate-console-plugin',
+  keySeparator: false,
+  // see below for more details
+  lexers: {
+    default: ['JavascriptLexer'],
+    handlebars: ['HandlebarsLexer'],
+
+    hbs: ['HandlebarsLexer'],
+    htm: ['HTMLLexer'],
+
+    html: ['HTMLLexer'],
+    js: ['JavascriptLexer'], // if you're writing jsx inside .js files, change this to JsxLexer
+    json: [CustomJSONLexer],
+    jsx: ['JsxLexer'],
+    mjs: ['JavascriptLexer'],
+    ts: ['JavascriptLexer'],
+
+    tsx: ['JsxLexer'],
+  },
+  locales: ['en', 'es', 'fr', 'ja', 'ko', 'zh'],
+  namespaceSeparator: '~',
+  reactNamespace: false,
+  sort: true,
+
+  useKeysAsDefaultValue: true,
+};
